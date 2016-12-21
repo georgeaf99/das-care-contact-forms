46534b72f23243e3dc9bc6e9bdd2a88621a9ef49
George Farcasiu <farcasiu.george@gmail.com>
2016-12-21 14:28:52 -0700

    Consolidate grouped responses storing the latest data
    close #4

--- report_generator.py
+++ report_generator.py
@@ -1,5 +1,8 @@
+import datetime
+import functools
 import itertools
 import logging
+import re
 
 import service
 
@@ -7,6 +10,33 @@ logger = logging.getLogger('das-care-contact-forms-logger')
 
 address_extractor = {
     "V1": lambda resp: resp.get('Street Address')
+}[service.forms_version]
+
+
+def convert_timestamp(timestamp_string):
+    """Utility function that converts a timestamp string to a datetime string
+
+    @timestamp_string The string to create a `datetime` object from
+    @return a `datetime` object
+    """
+    timestamp_regex = r"(?P<month>\d+)/(?P<day>\d+)/(?P<year>\d+)\s(?P<hour>\d+)\:(?P<minute>\d+)\:(?P<second>\d+)"
+
+    match = re.search(timestamp_regex, timestamp_string)
+
+    return datetime.datetime(**dict((
+        (key, int(val))
+        for key, val in match.groupdict().items()
+    )))
+
+
+def normalize_V1(resp):
+    resp['Timestamp'] = convert_timestamp(resp['Timestamp'])
+
+    return resp
+
+
+normalize = {
+    'V1': normalize_V1
 }[service.forms_version]
 
 
@@ -20,12 +50,13 @@ def format_responses(responses_wks):
     schema = responses_wks.row_values(1)
     row_values = responses_wks.get_all_values()[1:]
 
+    # Map responses to the schema and normalize each response
     mapped_response_data = [
-        dict((
+        normalize(dict((
             (col_name, cell_val)
             for col_name, cell_val in zip(schema, response)
             if cell_val != ''
-        ))
+        )))
         for response in row_values
     ]
 
@@ -45,10 +76,31 @@ def group_responses_by_address(formatted_resps):
     return dict(
         (address, list(resps))
         for address, resps in itertools.groupby(
-            sorted(formatted_resps, key=address_extractor),
+            # Sort the responses by address and then timestamp
+            sorted(
+                formatted_resps,
+                # Extracts a tuple that will be compared from left to right
+                key=lambda resp: (address_extractor(resp), resp['Timestamp'])
+            ),
             key=address_extractor
         )
     )
+
+def compress_grouped_responses(grouped_resps):
+    """Compress grouped responses to dislpay the most recent information
+
+    @param grouped_resps Responses grouped by address and sorted by timestamp to be compressed
+    @return A dictionary mapping addresses to compressed responses
+    """
+    compressed_responses = {}
+    for address, responses in grouped_resps.items():
+        compressed = {}
+        for resp in responses:
+            compressed.update(resp)
+
+        compressed_responses[address] = compressed
+
+    return compressed_responses
 
 
 if __name__ == "__main__":
@@ -56,3 +108,4 @@ if __name__ == "__main__":
 
     formatted_resps = format_responses(responses_wks)
     grouped_resps = group_responses_by_address(formatted_resps)
+    compressed_grouped_resps = compressed_grouped_resps(grouped_resps)
--- tests/test_report_generator.py
+++ tests/test_report_generator.py
@@ -1,3 +1,5 @@
+import datetime
+
 import pytest
 
 import report_generator
@@ -16,8 +18,8 @@ class MockWorksheet:
 
 def test_format_responses():
     mock_wks = MockWorksheet([
-        ['Street Address', 'b', 'c', 'd'],
-        ['1',              '',  '2', '3'],
+        ['Street Address', 'Timestamp',          'b', 'c', 'd'],
+        ['1',              '10/4/2016 11:47:55', '',  '2', '3'],
     ])
 
     formatted_resps = report_generator.format_responses(mock_wks)
@@ -27,6 +29,11 @@ def test_format_responses():
     resp = formatted_resps[0]
 
     assert resp['Street Address'] == '1'
+    assert resp['Timestamp'] == datetime.datetime(
+        month=10, day=4, year=2016,
+        hour=11, minute=47, second=55
+    )
+
     assert resp.get('b') is None
     assert resp['c'] == '2'
     assert resp['d'] == '3'
@@ -34,10 +41,10 @@ def test_format_responses():
 
 def test_group_responses_by_address():
     mock_wks = MockWorksheet([
-        ['Street Address', 'b', 'c', 'd'],
-        ['address_one',    '',  '1', '2'],
-        ['address_two',    '',  '3', '4'],
-        ['address_one',    '',  '5', '6'],
+        ['Street Address', 'Timestamp',          'b', 'c', 'd'],
+        ['address_one',    '10/4/2016 15:09:24', '',  '1', '2'],
+        ['address_two',    '10/4/2016 11:47:55', '',  '3', '4'],
+        ['address_one',    '12/1/2017 06:03:22', '',  '5', '6'],
     ])
 
     formatted_resps = report_generator.format_responses(mock_wks)
@@ -45,32 +52,69 @@ def test_group_responses_by_address():
 
     assert grouped_resps == {
         "address_one": [
-            {'Street Address': 'address_one', 'c': '1', 'd': '2'},
-            {'Street Address': 'address_one', 'c': '5', 'd': '6'},
+            {
+                'Street Address': 'address_one',
+                'Timestamp': datetime.datetime(
+                    month=10, day=4, year=2016,
+                    hour=15, minute=9, second=24
+                ),
+                'c': '1',
+                'd': '2',
+            },
+            {
+                'Street Address': 'address_one',
+                'Timestamp': datetime.datetime(
+                    month=12, day=1, year=2017,
+                    hour=6, minute=3, second=22
+                ),
+                'c': '5',
+                'd': '6',
+            },
         ],
         "address_two": [
-            {'Street Address': 'address_two', 'c': '3', 'd': '4'},
+            {
+                'Street Address': 'address_two',
+                'Timestamp': datetime.datetime(
+                    month=10, day=4, year=2016,
+                    hour=11, minute=47, second=55
+                ),
+                'c': '3',
+                'd': '4',
+            },
         ],
     }
 
 
 def test_compress_grouped_responses():
     mock_wks = MockWorksheet([
-        ['Street Address', 'b', 'c', 'd'],
-        ['address_one',    '',  '1', '2'],
-        ['address_two',    '',  '3', '4'],
-        ['address_one',    '0',  '5', '6'],
+        ['Street Address', 'Timestamp',          'b', 'c', 'd'],
+        ['address_one',    '10/4/2016 15:09:24', '',  '1', '2'],
+        ['address_two',    '10/4/2016 11:47:55', '',  '3', '4'],
+        ['address_one',    '12/1/2017 06:03:22', 'x', '5', '6'],
     ])
 
     formatted_resps = report_generator.format_responses(mock_wks)
     grouped_resps = report_generator.group_responses_by_address(formatted_resps)
+    compressed_resps = report_generator.compress_grouped_responses(grouped_resps)
 
-    assert grouped_resps == {
-        "address_one": [
-            {'Street Address': 'address_one', 'c': '1', 'd': '2'},
-            {'Street Address': 'address_one', 'c': '5', 'd': '6'},
-        ],
-        "address_two": [
-            {'Street Address': 'address_two', 'c': '3', 'd': '4'},
-        ],
+    assert compressed_resps == {
+        "address_one": {
+            'Street Address': 'address_one',
+            'Timestamp': datetime.datetime(
+                month=12, day=1, year=2017,
+                hour=6, minute=3, second=22
+            ),
+            'b': 'x',
+            'c': '5',
+            'd': '6',
+        },
+        "address_two": {
+            'Street Address': 'address_two',
+            'Timestamp': datetime.datetime(
+                month=10, day=4, year=2016,
+                hour=11, minute=47, second=55
+            ),
+            'c': '3',
+            'd': '4'
+        },
     }
